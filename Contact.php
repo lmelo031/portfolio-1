cd3b6a074392407e578a229c42770798843cb5b9
Laura Melo <lamelob99@hotmail.com>
2022-02-23 17:33:37 -0500

    complete

--- Contact.php
+++ Contact.php
@@ -1,3 +1,32 @@
-<?php
-echo($_POST["name"]);
-?>
+
+<html>
+<head>
+<!--Font links-->
+<link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet'>
+
+<!--StyleSheet-->
+<style>
+p {
+font-family:'Times New Roman', Times, serif;
+text-align: center;
+font-size: 135%;
+}
+
+h1 {
+font-family: Lobster;
+Text-align: center;
+}
+
+body {
+Background-color:#e5c2b4;
+}
+</style>
+
+</head>
+<p>Thank you for contacting us!</p>
+Hello! <?php echo $_POST["name"];?><br>
+<b>We will sent you an email confirmation to:</b>
+ <?php echo $_POST["email"];?>
+ <?php echo $_POST["appt"]; ?>
+ 
